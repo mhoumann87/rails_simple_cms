479e25ac2b1af2fc50417858459462d315223f38
Michael Houmann <mhoumann87@gmail.com>
2019-03-23 08:56:01 +0100

    Set up functions for create and update page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,9 @@ class PagesController < ApplicationController
 
   layout "admin"
 
+  before_action :find_subjects, :only => [:new, :create, :edit, :update]
+  before_action :set_page_count, :only => [:new, :create, :edit, :update]
+
   def index
     @pages = Page.sorted
   end
@@ -12,9 +15,7 @@ class PagesController < ApplicationController
 
   def new
     @page = Page.new
-    @page_count = Page.count + 1
-    @subjects = Subject.sorted 
-  end
+   end
 
   def create
     # Instantiate a new object using form parameters
@@ -26,16 +27,12 @@ class PagesController < ApplicationController
       redirect_to(pages_path)
     else
     # It save fails, redisplay the form so user can fix problelms
-      @page_count = Page.count
-      @subjects = Subject.sorted
       render("new")
     end
   end
 
   def edit
     @page = Page.find(params[:id])
-    @page_count = Page.count + 1
-    @subjects = Subject.sorted
   end
 
   def update
@@ -45,8 +42,6 @@ class PagesController < ApplicationController
       flash[:notice] = "Page updated successfully"
       redirect_to(page_path(@page))
     else
-      @page_count = Page.count
-    @subjects = Subject.sorted
       redirect_to("edit")
     end
   end
@@ -66,5 +61,17 @@ class PagesController < ApplicationController
 
   def page_params
     params.require(:page).permit(:name, :subject_id, :permalink, :position, :visible)
+  end
+
+  def find_subjects
+    @subjects = Subject.sorted
+  end
+
+  def set_page_count
+    @page_count = Page.count
+
+    if params[:action] == "new" || params[:action] == "create"
+      @page_count += 1
+    end
   end
 end
